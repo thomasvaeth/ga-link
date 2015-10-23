eacc42590edc957c93a457caaa08425164d22687
thomasvaeth <thomas.vaeth@gmail.com>
2015-10-22 19:01:38 -0700

    Working page with database. Adding hash.

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 var express = require('express');
 var app = express();
+var db = require('./models');
 
 var ejsLayouts = require('express-ejs-layouts');
 app.use(ejsLayouts);
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
 	res.render('index');
+});
+
+app.post('/links/', function(req, res) {
+	var data = req.body.q;
+	db.link.create({url: data, hash: ''}).then(function(link) {
+		res.redirect('/links/' + link.id);
+	});
+});
+
+app.get('/links/:item', function(req, res) {
+	var item = parseInt(req.params.item);
+	db.link.findById(item).then(function(item) {
+		res.render('links/show', {item: item});
+	});
 });
 
 app.listen(3000);
---
+++ testDB.js
@@ -0,0 +1,12 @@
+var db = require('./models');
+
+// db.link.create({
+// 	url: 'http://google.com',
+// 	hash: ' '
+// }).then(function(link) {
+// 	console.log(link);
+// });
+
+db.link.find({where: {id: 1}}).then(function(link) {
+	console.log(link.url);
+});
